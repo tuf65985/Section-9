5ef8f876a7e8cdb71f8ea3c3e9130a9a908f3068
unknown <C82294@LIBL0926.internal.cigna.com>
2017-06-15 16:41:33 -0400

    9.101 Injecting Services into Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import {AccountsService} from './accounts.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [AccountsService]
 })
 export class AppComponent {
 accounts: {name: string, status: string}[]=[];
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -5,7 +5,7 @@ import {AccountsService} from '../accounts.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService, ]
+ // providers: [LoggingService, ]
 })
 export class NewAccountComponent {
  
@@ -18,7 +18,7 @@ export class NewAccountComponent {
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    //this.loggingService.logStatusChange(accountStatus);
    // console.log('A server status changed, new status: ' + accountStatus);
   }
 }
