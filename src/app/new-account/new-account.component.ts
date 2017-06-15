e5adf784a9f778b77e2beeab1d45a562545b91f2
unknown <C82294@LIBL0926.internal.cigna.com>
2017-06-15 14:52:13 -0400

    9.96Creating a Logging Service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-
+import { LoggingService } from '../logging.service'
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
@@ -13,6 +13,6 @@ export class NewAccountComponent {
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+   // console.log('A server status changed, new status: ' + accountStatus);
   }
 }
