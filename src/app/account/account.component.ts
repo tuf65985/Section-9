36d38e6772c30aca27f960c2212f28f50e61ac4b
unknown <C82294@LIBL0926.internal.cigna.com>
2017-06-15 16:32:44 -0400

    9.100 Hierarchal Injections

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input,  } from '@angular/core';
 import { LoggingService } from '../logging.service'
+import {AccountsService} from '../accounts.service';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
@@ -9,14 +10,16 @@ import { LoggingService } from '../logging.service'
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
- constructor(private loggingService: LoggingService)
+ 
+  constructor(private loggingService: LoggingService,
+   private accountsService: AccountsService)
   {
 
   }
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.accountsService.updateStatus(this.id,status);
+    this.loggingService.logStatusChange(status);
+   
   }
 }
